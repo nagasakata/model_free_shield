889861035f3a43f0d24c47ef3f9435cb2b306fdc
nagasakata <takekinagasaka1231@gmail.com>
2023-05-11 01:34:21 +0900

    commit 5/11 env

--- benchmarks/pacman/pacman_specifications.py
+++ benchmarks/pacman/pacman_specifications.py
@@ -17,17 +17,17 @@ class Observations(Enum):
 
 
 
-right_go = f'G ({Observations.RIGHT_APPROACH} → X ((!{Observations.RIGHT_GO}) ∨ ({Observations.NO_MOVE})))'
-left_go = f'G ({Observations.LEFT_APPROACH} → X ((!{Observations.LEFT_GO}) ∨ ({Observations.NO_MOVE})))'
-up_go = f'G ({Observations.UP_APPROACH} → X ((!{Observations.UP_GO}) ∨ ({Observations.NO_MOVE})))'
-down_go = f'G ({Observations.DOWN_APPROACH} → X ((!{Observations.DOWN_GO}) ∨ ({Observations.NO_MOVE})))'
+right_go = f'G ({Observations.RIGHT_APPROACH} → X ((!{Observations.RIGHT_GO}) ∧ ({Observations.NO_MOVE})))'
+left_go = f'G ({Observations.LEFT_APPROACH} → X ((!{Observations.LEFT_GO}) ∧ ({Observations.NO_MOVE})))'
+up_go = f'G ({Observations.UP_APPROACH} → X ((!{Observations.UP_GO}) ∧ ({Observations.NO_MOVE})))'
+down_go = f'G ({Observations.DOWN_APPROACH} → X ((!{Observations.DOWN_GO}) ∧ ({Observations.NO_MOVE})))'
 
 enemy_approach = f'({right_go}) ∧ ({left_go}) ∧ ({up_go}) ∧ ({down_go})'
 
-right_wall = f'G ({Observations.RIGHT_WALL} → X ((!{Observations.RIGHT_GO}) ∨ ({Observations.NO_MOVE})))'
-left_wall = f'G ({Observations.LEFT_WALL} → X ((!{Observations.LEFT_GO}) ∨ ({Observations.NO_MOVE})))'
-up_wall = f'G ({Observations.UP_WALL} → X ((!{Observations.UP_GO}) ∨ ({Observations.NO_MOVE})))'
-down_wall = f'G ({Observations.DOWN_WALL} → X ((!{Observations.DOWN_GO}) ∨ ({Observations.NO_MOVE})))'
+right_wall = f'G ({Observations.RIGHT_WALL} → X ((!{Observations.RIGHT_GO}) ∧ ({Observations.NO_MOVE})))'
+left_wall = f'G ({Observations.LEFT_WALL} → X ((!{Observations.LEFT_GO}) ∧ ({Observations.NO_MOVE})))'
+up_wall = f'G ({Observations.UP_WALL} → X ((!{Observations.UP_GO}) ∧ ({Observations.NO_MOVE})))'
+down_wall = f'G ({Observations.DOWN_WALL} → X ((!{Observations.DOWN_GO}) ∧ ({Observations.NO_MOVE})))'
 
 wall_is_there = f'({right_wall}) ∧ ({left_wall}) ∧ ({up_wall}) ∧ ({down_wall})'
 
