e1211046e995b98d90836b1b96f1ca5c522a7922
nagasakata <takekinagasaka1231@gmail.com>
2023-05-11 01:30:14 +0900

    commit 5/11 env

--- benchmarks/pacman/pacman_specifications.py
+++ benchmarks/pacman/pacman_specifications.py
@@ -17,25 +17,25 @@ class Observations(Enum):
 
 
 
-right_go = f'G ({Observations.RIGHT_APPROACH} → X (!{Observations.RIGHT_GO}))'
-left_go = f'G ({Observations.LEFT_APPROACH} → X (!{Observations.LEFT_GO}))'
-up_go = f'G ({Observations.UP_APPROACH} → X (!{Observations.UP_GO}))'
-down_go = f'G ({Observations.DOWN_APPROACH} → X (!{Observations.DOWN_GO}))'
+right_go = f'G ({Observations.RIGHT_APPROACH} → X ((!{Observations.RIGHT_GO}) ∨ ({Observations.NO_MOVE})))'
+left_go = f'G ({Observations.LEFT_APPROACH} → X ((!{Observations.LEFT_GO}) ∨ ({Observations.NO_MOVE})))'
+up_go = f'G ({Observations.UP_APPROACH} → X ((!{Observations.UP_GO}) ∨ ({Observations.NO_MOVE})))'
+down_go = f'G ({Observations.DOWN_APPROACH} → X ((!{Observations.DOWN_GO}) ∨ ({Observations.NO_MOVE})))'
 
 enemy_approach = f'({right_go}) ∧ ({left_go}) ∧ ({up_go}) ∧ ({down_go})'
 
-right_wall = f'G ({Observations.RIGHT_WALL} → X (!{Observations.RIGHT_GO}))'
-left_wall = f'G ({Observations.LEFT_WALL} → X (!{Observations.LEFT_GO}))'
-up_wall = f'G ({Observations.UP_WALL} → X (!{Observations.UP_GO}))'
-down_wall = f'G ({Observations.DOWN_WALL} → X (!{Observations.DOWN_GO}))'
+right_wall = f'G ({Observations.RIGHT_WALL} → X ((!{Observations.RIGHT_GO}) ∨ ({Observations.NO_MOVE})))'
+left_wall = f'G ({Observations.LEFT_WALL} → X ((!{Observations.LEFT_GO}) ∨ ({Observations.NO_MOVE})))'
+up_wall = f'G ({Observations.UP_WALL} → X ((!{Observations.UP_GO}) ∨ ({Observations.NO_MOVE})))'
+down_wall = f'G ({Observations.DOWN_WALL} → X ((!{Observations.DOWN_GO}) ∨ ({Observations.NO_MOVE})))'
 
 wall_is_there = f'({right_wall}) ∧ ({left_wall}) ∧ ({up_wall}) ∧ ({down_wall})'
 
-no_move = f'G({Observations.NO_MOVE})'
+#no_move = f'G({Observations.NO_MOVE})'
 
 #remove
 #if_cant_go = f'G ((({Observations.RIGHT_APPROACH} ∨ {Observations.RIGHT_WALL}) ∧ ({Observations.LEFT_APPROACH} ∨ {Observations.LEFT_WALL}) ∧ ({Observations.UP_APPROACH} ∨ {Observations.UP_WALL}) ∧ ({Observations.DOWN_APPROACH} ∨ {Observations.DOWN_WALL})) → X (({Observations.RIGHT_GO}) ∨ ({Observations.LEFT_GO}) ∨ ({Observations.UP_GO}) ∨ ({Observations.DOWN_GO})))'
 
 
 def safety_formula():
-    return f'({enemy_approach}) ∧ ({wall_is_there}) ∧ ({no_move})'
+    return f'({enemy_approach}) ∧ ({wall_is_there})'
