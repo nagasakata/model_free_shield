7e718a348d0bb7a65ff26311ad141bf427afc072
nagasakata <takekinagasaka1231@gmail.com>
2023-05-18 14:17:21 +0900

    commit solve game

--- src/logic/solve_game.py
+++ src/logic/solve_game.py
@@ -13,6 +13,20 @@ def solve_game(game:SafetyGame):
     while True:
         winning_strategy = dict()
         winning_state = copy.copy(safe_state)
+
+        for (start, p1_now, p2_now) in game.transitions:
+            for  p1_action in action_1:
+                for p2_action in action_2:
+                    if (game.transitions(start, p1_now, p2_now), p1_action, p2_action) in game.transitions:
+                        if (start, p1_now) in winning_strategy:
+                            winning_strategy[(start, p1_now)].append(p2_action)
+                        else:
+                            winning_strategy[(start, p1_now)] = [p2_action]
+
+        break 
+            
+
+        '''
         for q in winning_state:
             for a1 in action_1:
                 for a2 in action_2:
@@ -26,7 +40,8 @@ def solve_game(game:SafetyGame):
         if safe_state == winning_state:
             break
         safe_state = winning_state
+        '''
 
-    #pprint.pprint(winning_strategy)
+    #cpprint.pprint(winning_strategy)
 
     return winning_state, winning_strategy
